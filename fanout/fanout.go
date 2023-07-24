ab8fa9661d885a35a43c9d2c44939b0541421d6d
fernando ocampo <fdocampo@gmail.com>
2023-07-24 17:22:37 +0200

    add fanout with semaphore

--- fanout/fanout.go
+++ fanout/fanout.go
@@ -3,6 +3,7 @@ package fanout
 import (
 	"context"
 	"fmt"
+	"runtime"
 )
 
 // Process fan out multiple values from different workers.
@@ -11,6 +12,28 @@ func Process(ctx context.Context, numberOfWorkers int) ([]string, error) {
 	// starting many copies as we have CPUs.
 	for i := 0; i < numberOfWorkers; i++ {
 		workers[i] = generateData(ctx)
+	}
+
+	result := make([]string, 0, numberOfWorkers)
+	for indx := range workers {
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("reading workers data: %w", ctx.Err())
+		case value := <-workers[indx]:
+			result = append(result, value)
+		}
+	}
+
+	return result, nil
+}
+
+func ProcessWithSemaphore(ctx context.Context, numberOfWorkers int) ([]string, error) {
+	workers := make([]<-chan string, numberOfWorkers)
+	semaphore := newSemaphore()
+
+	// starting many copies as number of workers we want.
+	for i := 0; i < numberOfWorkers; i++ {
+		workers[i] = generateDataWithSemaphore(ctx, semaphore)
 	}
 
 	result := make([]string, 0, numberOfWorkers)
@@ -37,4 +60,26 @@ func generateData(ctx context.Context) chan string {
 		}
 	}(ctx)
 	return valueStream
+}
+
+func generateDataWithSemaphore(ctx context.Context, semaphore chan bool) chan string {
+	valueStream := make(chan string, 1)
+	go func(ctx context.Context) {
+		defer close(valueStream)
+		semaphore <- true
+		{
+			select {
+			case <-ctx.Done():
+				return
+			case valueStream <- "value":
+			}
+		}
+		<-semaphore
+	}(ctx)
+	return valueStream
+}
+
+func newSemaphore() chan bool {
+	workersAvailable := runtime.GOMAXPROCS(0)
+	return make(chan bool, workersAvailable)
 }
