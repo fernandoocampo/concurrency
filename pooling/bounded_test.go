7a9522a40ce30c55a0f526180d7c348966e8d8aa
fernando ocampo <fdocampo@gmail.com>
2023-07-28 12:44:01 +0200

    add bounded work pooling pattern

---
+++ pooling/bounded_test.go
@@ -0,0 +1,38 @@
+package pooling_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/fernandoocampo/concurrency/pooling"
+)
+
+func TestBoundedWorkPooling(t *testing.T) {
+	// Given
+	timeout := 2 * time.Second
+	ctx, cancel := context.WithTimeout(context.TODO(), timeout)
+	defer cancel()
+	someWork := []string{"work", "work", "work", 2000: "work"}
+	wantedWorks := 4
+	wantedWorkWord := "WORK"
+
+	// When
+	got := pooling.DoBoundedWork(ctx, someWork)
+
+	// Then
+	if len(got) != len(someWork) {
+		t.Errorf("want: %d amount of work, but got: %d", len(someWork), len(got))
+	}
+
+	var gotWorkWithWork int
+	for _, v := range got {
+		if v == wantedWorkWord {
+			gotWorkWithWork++
+		}
+	}
+
+	if wantedWorks != gotWorkWithWork {
+		t.Errorf("want: %d works with word %q, but got: %d", wantedWorks, wantedWorkWord, gotWorkWithWork)
+	}
+}
--- pooling/pool.go
+++ pooling/pool.go
@@ -3,6 +3,7 @@ package pooling
 import (
 	"context"
 	"runtime"
+	"strings"
 	"sync"
 	"sync/atomic"
 )
@@ -32,6 +33,50 @@ func SumInput(ctx context.Context, numbers <-chan uint32) uint32 {
 	}
 
 	wg.Wait()
+
+	return result
+}
+
+func DoBoundedWork(ctx context.Context, work []string) []string {
+	goroutines := runtime.GOMAXPROCS(0)
+	wg := sync.WaitGroup{}
+	wg.Add(goroutines)
+	inputStream := make(chan string, goroutines)
+	resultStream := make(chan string, len(work))
+
+	for i := 0; i < goroutines; i++ {
+		go func(subctx context.Context) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case value, ok := <-inputStream:
+					if !ok {
+						return
+					}
+
+					resultStream <- strings.ToUpper(value)
+				}
+			}
+		}(ctx)
+	}
+
+	for _, v := range work {
+		word := v
+		inputStream <- word
+	}
+
+	close(inputStream)
+
+	wg.Wait()
+
+	close(resultStream)
+
+	result := make([]string, 0, len(work))
+	for v := range resultStream {
+		result = append(result, v)
+	}
 
 	return result
 }
